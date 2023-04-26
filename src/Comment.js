c049790763fe59a603408e29740d4363ec6125dc
crackerFactory <lee.mander@gmail.com>
2023-04-26 16:42:23 +0100

    Style comment element

--- src/Comment.js
+++ src/Comment.js
@@ -8,17 +8,19 @@ export default function Comment(props) {
   return (
     <article className="comment">
       <div>
-        <div className="comment__score-controls">
-          <button /*onClick={incrementScore}*/>+</button>
-          <p>{score}</p>
-          <button /*onClick={decrementScore}*/>-</button>
+        <div className="comment__score-controls-wrapper">
+          <div className="comment__score-controls">
+            <button /*onClick={incrementScore}*/>+</button>
+            <p>{score}</p>
+            <button /*onClick={decrementScore}*/>-</button>
+          </div>
         </div>
       </div>
       <div>
         <div className="comment__header">
           <img className="comment__user-pic" src={userPic} />
           <span className="comment__username">{username}</span>
-          <span>{timeAdded}</span>
+          <span className="comment_time-added">{timeAdded}</span>
           <button className="comment__reply-button" /*onClick={addReply}*/>
             Reply
           </button>
